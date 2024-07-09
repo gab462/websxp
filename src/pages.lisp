16647f55ae0f1be294bb90a96efe8a156b8890de
gab462 <gustavoaraujob1+1@gmail.com>
2024-07-08 21:39:53 -0300

    Improve page style

--- src/pages.lisp
+++ src/pages.lisp
@@ -15,11 +15,11 @@
     (:head
      (:title ,title)
      (:link :href "https://cdn.jsdelivr.net/npm/@picocss/pico@2.0.6/css/pico.classless.min.css"
-             :rel "stylesheet"))
+            :rel "stylesheet"))
     (:body
      (:script :src "https://unpkg.com/htmx.org@2.0.0"
               :type "text/javascript")
-     (:h1 ,title)
+     (:header (:h1 ,title))
      ,body)))
 
 (defun render-page (title page)
@@ -32,14 +32,17 @@
       (spinneret:interpret-html-tree element)))
 
 (defun contacts-table (contacts)
-  `(:form
-    (:table
-     (:thead
-      ,@(mapcar ^(list :th _) '("Name" "Email" "Status"))
-      (:th))
-     (:tbody :hx-confirm "Are you sure?" :hx-target "closest tr" :hx-swap "outerHTML"
-             ,@(mapcar ^(destructuring-bind (&key id name email status) _
-                          `(:tr
-                            ,@(mapcar (lambda (field) (list :td field)) (list name email status))
-                            (:td (:button :hx-delete ,(format nil "/contact/~A" id) "Delete"))))
-                       contacts)))))
+  `(:main
+    (:section
+     :id "table"
+     (:form
+      (:table
+       (:thead
+        ,@(mapcar ^(list :th _) '("Name" "Email" "Status"))
+        (:th))
+       (:tbody :hx-confirm "Are you sure?" :hx-target "closest tr" :hx-swap "outerHTML"
+               ,@(mapcar ^(destructuring-bind (&key id name email status) _
+                            `(:tr
+                              ,@(mapcar (lambda (field) (list :td field)) (list name email status))
+                              (:td (:button :hx-delete ,(format nil "/contact/~A" id) "Delete"))))
+                         contacts)))))))
